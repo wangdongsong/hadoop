86f80f41793454d1a25feb1816076bb672d2bc42
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-17 22:53:15 +0800

    MapReduce Top N 唯一键

---
+++ src/main/java/com/wds/dataalgorithms/topN/PrimaryKeyTopNMRDriver.java
@@ -0,0 +1,14 @@
+package com.wds.dataalgorithms.topN;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ *
+ * Created by wangdongsong1229@163.com on 2017/7/17.
+ */
+public class PrimaryKeyTopNMRDriver {
+
+    public class TopNMapper extends Mapper{
+
+    }
+}
