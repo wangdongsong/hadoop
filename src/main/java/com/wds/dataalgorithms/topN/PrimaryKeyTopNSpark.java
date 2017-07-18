28e1c4c13ec85f3c3ee493f6c6d496165931a06a
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-18 22:18:53 +0800

    Spark Top N 唯一键

--- src/main/java/com/wds/dataalgorithms/topN/PrimaryKeyTopNSpark.java
+++ src/main/java/com/wds/dataalgorithms/topN/PrimaryKeyTopNSpark.java
@@ -4,8 +4,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Created by wangdongsong1229@163.com on 2017/7/17.
@@ -36,7 +40,73 @@ public class PrimaryKeyTopNSpark {
                 String[] tokens = s.split(",");
                 return new Tuple2<>(tokens[0], Integer.parseInt(tokens[1]));
             }
-        })
+        });
+
+        //Step5 函数式写法
+        pairs = lines.mapToPair((s) ->{
+            String[] tokens = s.split(",");
+            return new Tuple2<>(tokens[0], Integer.parseInt(tokens[1]));
+        });
+
+        //step6 为各个输入分区创建本地的Top N列表
+        JavaRDD<SortedMap<Integer, String>> partitions = pairs.mapPartitions(new FlatMapFunction<Iterator<Tuple2<String, Integer>>, SortedMap<Integer, String>>() {
+            @Override
+            public Iterator<SortedMap<Integer, String>> call(Iterator<Tuple2<String, Integer>> tuple2Iterator) throws Exception {
+                SortedMap<Integer, String> top10 = new TreeMap<Integer, String>();
+                while (tuple2Iterator.hasNext()) {
+                    Tuple2<String, Integer> tuple = tuple2Iterator.next();
+                    top10.put(tuple._2, tuple._1);
+                    if (top10.size() > 10) {
+                        top10.remove(top10.firstKey());
+                    }
+                }
+
+                return Collections.singletonList(top10).iterator();
+            }
+
+        });
+
+        //step7 使用collect创建最终的top 10列表，该步有替换方案，使用JavaRDD.reduce
+        SortedMap<Integer, String> finalTop10 = new TreeMap<>();
+        List<SortedMap<Integer, String>> list = partitions.collect();
+        list.forEach((l) ->{
+            l.entrySet().forEach((entry) ->{
+                finalTop10.put(entry.getKey(), entry.getValue());
+
+                if (finalTop10.size() > 10){
+                    finalTop10.remove(finalTop10.firstKey());
+                }
+            });
+        });
+
+        //Step7的替换方案，使用JavaRDD.reduce
+        SortedMap<Integer, String> finalTop10Reduce = partitions.reduce((m1, m2) -> {
+            SortedMap<Integer, String> top10 = new TreeMap<>();
+            m1.entrySet().forEach((map) ->{
+                top10.put(map.getKey(), map.getValue());
+                if (top10.size() > 10){
+                    top10.remove(top10.firstKey());
+                }
+            });
+
+            m2.entrySet().forEach((map) ->{
+                top10.put(map.getKey(), map.getValue());
+                if (top10.size() > 10){
+                    top10.remove(top10.firstKey());
+                }
+            });
+            return top10;
+        });
+
+        //step8 输出最终结果
+        finalTop10.forEach((key, value) ->{
+            System.out.println(key + ", " + value);
+        });
+        System.out.println("---Reduce---");
+        finalTop10Reduce.forEach((key, value) ->{
+            System.out.println(key + ", " + value);
+        });
+
     }
 
 }
