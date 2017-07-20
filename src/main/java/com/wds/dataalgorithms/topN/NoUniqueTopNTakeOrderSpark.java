a612024c44c8d27ce3d16a81a52c8647f2c02e76
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-20 22:41:05 +0800

    commit non unique top n use takeOrdered method

--- src/main/java/com/wds/dataalgorithms/topN/NoUniqueTopNTakeOrderSpark.java
+++ src/main/java/com/wds/dataalgorithms/topN/NoUniqueTopNTakeOrderSpark.java
@@ -1,5 +1,13 @@
 package com.wds.dataalgorithms.topN;
 
+import com.wds.dataalgorithms.utils.SparkUtils;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.List;
+
 /**
  * 使用takeOrdered的Spark Top 10 解决方案
  *
@@ -17,4 +25,45 @@ package com.wds.dataalgorithms.topN;
  * Created by wangdongsong1229@163.com on 2017/7/20.
  */
 public class NoUniqueTopNTakeOrderSpark {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1){
+            System.err.println("Usage: SecondarySortSpark <Input-Path><topNum>");
+            System.exit(1);
+        }
+
+        final String inputPath = args[0];
+        final int N = Integer.parseInt(args[1]);
+
+        JavaSparkContext ctx = SparkUtils.createJavaSparkContext("top-10-use-takeOrdered");
+
+        JavaRDD<String> lines = ctx.textFile(inputPath, 1);
+        lines.saveAsTextFile("/output/1");
+
+        JavaRDD<String> rdd = lines.coalesce(3);
+
+        //映射（K，V）
+        JavaPairRDD<String, Integer> kv = rdd.mapToPair((inputS) ->{
+            String[] tokens = inputS.split(",");
+            return new Tuple2<String, Integer>(tokens[0], Integer.parseInt(tokens[1]));
+        });
+        kv.saveAsTextFile("/output/2");
+
+        //归约
+        JavaPairRDD<String, Integer> uniqueKeys = kv.reduceByKey((num1, num2) -> {
+            return num1 + num2;
+        });
+        uniqueKeys.saveAsTextFile("/output/3");
+
+        //takeOrdered and print result
+        uniqueKeys.takeOrdered(N, (t1, t2) -> {
+            return t1._1().compareTo(t2._1());
+        }).forEach((entry) -> {
+            System.out.println(entry._2 + "--" + entry._1);
+        });
+
+    }
+
+
+
 }
