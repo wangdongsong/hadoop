28729998ea9b12bcf7bffee0a2f127dfe7fd93b4
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-26 06:22:38 +0800

    左外连接Spark，step4

--- src/main/java/com/wds/dataalgorithms/leftouterjoin/LeftOuterJoinSpark.java
+++ src/main/java/com/wds/dataalgorithms/leftouterjoin/LeftOuterJoinSpark.java
@@ -1,6 +1,10 @@
 package com.wds.dataalgorithms.leftouterjoin;
 
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import redis.clients.jedis.Tuple;
+import scala.Tuple2;
 
 /**
  * Created by wangdongsong1229@163.com on 2017/7/25.
@@ -21,5 +25,12 @@ public class LeftOuterJoinSpark {
         JavaSparkContext ctx = new JavaSparkContext();
 
         //step4 为用户创建一个JavaRDD
+        JavaRDD<String> users = ctx.textFile(usersInputFile, 1);
+        JavaPairRDD<String, Tuple2<String, String>> userRdd = users.mapToPair((string) ->{
+            String[] userRecord = string.split("\t");
+            Tuple2<String, String> location = new Tuple2<>("L", userRecord[1]);
+            return new Tuple2<String, Tuple2<String, String>>(userRecord[0], location);
+        });
+
     }
 }
