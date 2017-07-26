85abafc7c07ec34d39a117142fac942221353346
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-27 06:50:51 +0800

    step2 input step3 context

--- src/main/java/com/wds/dataalgorithms/leftouterjoin/LeftOuterJoinSparkUseLeftOuterJoin.java
+++ src/main/java/com/wds/dataalgorithms/leftouterjoin/LeftOuterJoinSparkUseLeftOuterJoin.java
@@ -1,5 +1,7 @@
 package com.wds.dataalgorithms.leftouterjoin;
 
+import org.apache.spark.api.java.JavaSparkContext;
+
 /**
  * 使用LeftOuterJoin的Spark实现，MapReduce没有提供类似的方法
  *
@@ -12,5 +14,18 @@ package com.wds.dataalgorithms.leftouterjoin;
  * Created by wangdongsong1229@163.com on 2017/7/27.
  */
 public class LeftOuterJoinSparkUseLeftOuterJoin {
+    public static void main(String[] args) {
+        //setp2 读取输入参数
+        if (args.length > 2){
+            System.err.println("Usage: LeftOuterJoinSparkUseLeftOuterJoin <users> <transactions>");
+            System.exit(0);
+        }
+        String usersInputFile = args[0]; //输入文件
+        String transactionsInputFile = args[1];
+        System.out.println("users = " + usersInputFile);
+        System.out.println("transactions = " + transactionsInputFile);
 
+        //step3 创建JavaSparkContext对象
+        JavaSparkContext ctx = new JavaSparkContext();
+    }
 }
