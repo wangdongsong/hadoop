ac49cb721392c61997e5256a573903e080f54950
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-27 18:02:13 +0800

    Step4、5

--- src/main/java/com/wds/dataalgorithms/mba/MBASpark.java
+++ src/main/java/com/wds/dataalgorithms/mba/MBASpark.java
@@ -1,7 +1,11 @@
 package com.wds.dataalgorithms.mba;
 
+import org.apache.commons.math3.util.Combinations;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -22,12 +26,24 @@ public class MBASpark {
             System.exit(0);
         }
         String transactionsFileName = args[0];
-        
+
         //step3 创建Spark上下文
-        JavaSparkContext cts = createJavaSparkContext();
+        JavaSparkContext ctx = createJavaSparkContext();
 
         //step4 从HDFS读取所有交易并创建第一个RDD
+        JavaRDD<String> transactions = ctx.textFile(transactionsFileName, 1);
+        transactions.saveAsTextFile("/output/rules/output/1");
+
         //step5 生成频繁模式(map阶段）
+        JavaPairRDD<List<String>, Integer> patterns = transactions.flatMapToPair((tranString) ->{
+            List<String> list = toList(tranString);
+            List<List<String>> combinations = Combination.findSortedCombinations(list);
+            List<Tuple2<List<String>, Integer>> result = new ArrayList<>();
+            combinations.stream().filter((s) -> s.size() > 0).forEach(combList -> result.add(new Tuple2<List<String>, Integer>(combList, 1)));
+            return result.iterator();
+        });
+
+        patterns.saveAsTextFile("/output/rules/ouput/2");
         //step6 组合/归约模式(reduce阶段）
         //step7 生成所有子模式(map阶段2）
         //step8 生成关联规则（reduce阶段2)
