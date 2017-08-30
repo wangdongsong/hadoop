034e310e01655b246bd945baddcdd116892276d9
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-31 06:32:57 +0800

    共同好友

--- src/main/java/com/wds/dataalgorithms/commonfriends/FindCommonFriendsSpark.java
+++ src/main/java/com/wds/dataalgorithms/commonfriends/FindCommonFriendsSpark.java
@@ -1,14 +1,16 @@
 package com.wds.dataalgorithms.commonfriends;
 
+import com.google.common.collect.Sets;
+import com.google.gson.internal.Streams;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * 查找共同好友 Spark解决方案
@@ -36,6 +38,7 @@ public class FindCommonFriendsSpark {
 
         //Step5 将JavaRDD<String>映射到键-值对
         //应用映射器
+        //其中Key=Tuple<user1, user2>， value=好友列表
         JavaPairRDD<Tuple2<Long, Long>, Iterable<Long>> pairs = records.flatMapToPair((s) ->{
             String[] tokens = s.split(",");
             long person = Long.parseLong(tokens[0]);
@@ -57,17 +60,69 @@ public class FindCommonFriendsSpark {
             });
             return result.iterator();
         });
+        //Debug step5
+        pairs.collect().forEach((t2) -> {
+            System.out.println("debug5 key=" + t2._1() + " \t value=" + t2._2());
+        });
 
-
-
-        //其中Key=Tuple<user1, user2>， value=好友列表
         //Step6 将(key=Tuple<u1, u2>, value=List<friends>对归约为(key=Tuple<u1, u2>, value=List<<ListFriends>>)
+        JavaPairRDD<Tuple2<Long, Long>, Iterable<Iterable<Long>>> grouped = pairs.groupByKey();
+        grouped.collect().forEach((t2) -> {
+            System.out.println("debug5 key=" + t2._1() + " \t value=" + t2._2());
+        });
+
         //Step7 利用所有List<List<Long>>的交集查找共同好友
+        JavaPairRDD<Tuple2<Long, Long>, Iterable<Long>> commonFriends = grouped.mapValues((s) ->{
+            int size = 0;
+            Map<Long, Integer> countCommon = new HashMap<>();
+
+            for (Iterable<Long> iter : s) {
+                size++;
+                //TODO
+                List<Long> list = StreamSupport.stream(iter.spliterator(), false).collect(Collectors.toList());
+                if ((list == null) || (list.isEmpty())) {
+                    continue;
+                }
+
+                for (Long f : list) {
+                    Integer count = countCommon.get(f);
+                    if (count == null) {
+                        countCommon.put(f, 1);
+                    } else {
+                        countCommon.put(f, ++count);
+                    }
+                }
+            }
+
+            List<Long> finalCommonFriends = new ArrayList<>();
+            for (Map.Entry<Long, Integer> entry : countCommon.entrySet()) {
+                if (entry.getValue() == size) {
+                    finalCommonFriends.add(entry.getKey());
+                }
+            }
+
+            return finalCommonFriends;
+        });
+
+
+        //通过reduceByKey()转换器合并Step6和7
+        //输入pairs(K: Tuple2<Long, Long>, V: Iterable<Long>)
+        //输出commonfriends (K: String, V: Iterable<Long>)
+        //转换器 reduceByKey()
+        commonFriends = pairs.reduceByKey((a, b) ->{
+            final Set<Long> x = Sets.newHashSet(a);
+            return Sets.newHashSet(b).stream().filter((x::contains)).collect(Collectors.toSet());
+        });
 
     }
 
-    private static Tuple2<Long, Long> buildSortedTuple(long person, long l) {
-        return null;
+
+    private static Tuple2<Long, Long> buildSortedTuple(long a, long b) {
+        if (a < b) {
+            return new Tuple2<Long, Long>(a, b);
+        } else {
+            return new Tuple2<Long, Long>(b, a);
+        }
     }
 
 }
