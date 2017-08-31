285533143173e3e6a38d06a49322d4ec97e64988
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-01 06:58:37 +0800

    description

---
+++ src/main/java/com/wds/dataalgorithms/recommendation/FriendRecommendationSpark.java
@@ -0,0 +1,23 @@
+package com.wds.dataalgorithms.recommendation;
+
+/**
+ * 好友推荐
+ * Created by wangdongsong1229@163.com on 2017/9/1.
+ */
+public class FriendRecommendationSpark {
+
+    public static void main(String[] args) {
+        //Step2 处理输入参数
+
+        //Step3 创建Spark上下文
+
+        //Step4 读文件并创建RDD
+
+        //Step5 实现map函数
+
+        //Step6 实现reduce函数
+
+        //Step7 生成所需要的最终输出
+    }
+
+}
