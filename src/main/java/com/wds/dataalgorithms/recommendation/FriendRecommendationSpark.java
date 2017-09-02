38e0adca16579b9a6cca44e17e812e3b9e15e043
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-02 10:45:22 +0800

    Step5

--- src/main/java/com/wds/dataalgorithms/recommendation/FriendRecommendationSpark.java
+++ src/main/java/com/wds/dataalgorithms/recommendation/FriendRecommendationSpark.java
@@ -1,9 +1,11 @@
 package com.wds.dataalgorithms.recommendation;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -32,6 +34,33 @@ public class FriendRecommendationSpark {
         debug1.forEach(System.out::println);
 
         //Step5 实现map函数
+        JavaPairRDD<Long, Tuple2<Long, Long>> pairs = records.flatMapToPair((record) ->{
+            String[] tokens = record.split("\t");
+            long person = Long.parseLong(tokens[0]);
+            String friendsString = tokens[1];
+            String[] friendsTokenized = friendsString.split(",");
+
+            List<Long> friends = new ArrayList<>();
+            List<Tuple2<Long, Tuple2<Long, Long>>> mapperOutput = new ArrayList<Tuple2<Long, Tuple2<Long, Long>>>();
+
+            for (String friendAsString : friendsTokenized) {
+                long toUser = Long.parseLong(friendAsString);
+                friends.add(toUser);
+                Tuple2<Long, Long> directFriend = T2(toUser, -1L);
+                mapperOutput.add(T2(person, directFriend));
+            }
+
+            for (int i = 0; i < friends.size(); i++) {
+                for (int j = i + 1; j < friends.size(); j++) {
+                    Tuple2<Long, Long> possibleFriend1 = T2(friends.get(j), person);
+                    mapperOutput.add(T2(friends.get(i), possibleFriend1));
+                    Tuple2<Long, Long> possibleFriend2 = T2(friends.get(i), person);
+                    mapperOutput.add(T2(friends.get(i), possibleFriend2));
+                }
+            }
+
+            return mapperOutput.iterator();
+        });
 
         //Step6 实现reduce函数
 
