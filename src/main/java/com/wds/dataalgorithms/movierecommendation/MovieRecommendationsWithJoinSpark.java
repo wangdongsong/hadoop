6677933bbff82a158ad116a7d5cb6b2ef45302f5
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-03 09:17:16 +0800

    基于内容的推荐

---
+++ src/main/java/com/wds/dataalgorithms/movierecommendation/MovieRecommendationsWithJoinSpark.java
@@ -0,0 +1,14 @@
+package com.wds.dataalgorithms.movierecommendation;
+
+/**
+ * WithJoin
+ *
+ * Created by wangdongsong1229@163.com on 2017/9/2.
+ */
+public class MovieRecommendationsWithJoinSpark {
+
+    public static void main(String[] args) {
+
+    }
+
+}
