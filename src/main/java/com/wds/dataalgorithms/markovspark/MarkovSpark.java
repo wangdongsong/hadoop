b291b21b5969bd03e31076eb01992a5d3976437f
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-06 06:50:56 +0800

    Step4、5

--- src/main/java/com/wds/dataalgorithms/markovspark/MarkovSpark.java
+++ src/main/java/com/wds/dataalgorithms/markovspark/MarkovSpark.java
@@ -8,6 +8,9 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by wangdongsong1229@163.com on 2017/9/4.
@@ -40,8 +43,35 @@ public class MarkovSpark implements Serializable {
             Tuple2<Long, Integer> v = new Tuple2<>(date, amout);
             return new Tuple2<>(tokens[0], v);
         });
+        kv.saveAsTextFile("/output/markov/output/3");
 
+        //Step4 按CustomerID对交易分组
+        JavaPairRDD<String, Iterable<Tuple2<Long, Integer>>> customerRDD = kv.groupByKey();
+        customerRDD.saveAsTextFile("/output/markov/output/4");
 
+        //Step5 创建马尔可夫状态序列
+        JavaPairRDD<String, List<String>> stateSequence = customerRDD.mapValues((Iterable<Tuple2<Long, Integer>> dateAndAmout) -> {
+            List<Tuple2<Long, Integer>> list = toList(dateAndAmout);
+            Collections.sort(list, TupleComparatorAscending.INSTANCE);
+            return toStateSequence(list);
+        });
+    }
+
+    private static <U> U toStateSequence(List<Tuple2<Long, Integer>> list) {
+        return null;
+    }
+
+    private static List<Tuple2<Long, Integer>> toList(Iterable<Tuple2<Long, Integer>> dateAndAmout) {
+        return null;
+    }
+
+    static class TupleComparatorAscending implements Comparator<Tuple2<Long, Integer>>, Serializable {
+        final static TupleComparatorAscending INSTANCE = new TupleComparatorAscending();
+        @Override
+        public int compare(Tuple2<Long, Integer> t1, Tuple2<Long, Integer> t2) {
+            // return -t1._1.compareTo(t2._1);     // sorts RDD elements descending
+            return t1._1.compareTo(t2._1);         // sorts RDD elements ascending
+        }
     }
 
 }
