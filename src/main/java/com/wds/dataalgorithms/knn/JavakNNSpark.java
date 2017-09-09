7f57120f6979d724597a98b4e6daeab768cdd6e1
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-09 21:34:12 +0800

    上下文 分号字符

--- src/main/java/com/wds/dataalgorithms/knn/JavakNNSpark.java
+++ src/main/java/com/wds/dataalgorithms/knn/JavakNNSpark.java
@@ -1,5 +1,14 @@
 package com.wds.dataalgorithms.knn;
 
+import com.google.common.base.Splitter;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import javax.xml.parsers.SAXParser;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 /**
  * k-NN邻近算法
  * Created by wangdongsong1229@163.com on 2017/9/9.
@@ -22,6 +31,33 @@ public class JavakNNSpark {
         //Step8 按R中的r对距离分组
 
         //Step9 找出k个近邻并对r分类
+    }
+
+    /**
+     * 创建Spark上下文
+     * @return
+     * @throws Exception
+     */
+    private static JavaSparkContext createJavaSparkContext() throws Exception {
+        SparkConf conf = new SparkConf();
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        JavaSparkContext ctx = new JavaSparkContext(conf);
+        return ctx;
+    }
+
+    /**
+     *
+     * @param str 是一个逗号或分号分隔的double值列表，例如"1.1, 2.2, 3.3"或"1.1; 2.2; 3.3"
+     * @param delimiter 分隔符
+     * @return
+     */
+    private static List<Double> splitOnToListOfDouble(String str, String delimiter) {
+        Splitter splitter = Splitter.on(delimiter).trimResults();
+        if (splitter.split(str) == null) {
+            return null;
+        }
+
+        return StreamSupport.stream(splitter.split(str).spliterator(), false).map(Double::parseDouble).collect(Collectors.toList());
     }
 
 }
