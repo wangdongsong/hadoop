f7639338a34ca822864f0d6277a4b4146ff6ef2f
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-09 14:53:20 +0800

    knn class doc

---
+++ src/main/java/com/wds/dataalgorithms/knn/JavakNNSpark.java
@@ -0,0 +1,28 @@
+package com.wds.dataalgorithms.knn;
+
+/**
+ * k-NN邻近算法
+ * Created by wangdongsong1229@163.com on 2017/9/9.
+ */
+public class JavakNNSpark {
+
+    public static void main(String[] args) {
+        //Step2 处理输入参数
+
+        //Step3 创建上下文
+
+        //Step4 广播共享对象
+
+        //Step5 对查询和训练数据集创建RDD
+
+        //Step6 计算（R，S）的笛卡尔积
+
+        //Step7 找出R中的r与S中的s之间的距离distance(r, s）
+
+        //Step8 按R中的r对距离分组
+
+        //Step9 找出k个近邻并对r分类
+    }
+
+}
+
