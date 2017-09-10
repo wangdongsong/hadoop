cd0ef55e58824744b56d2c6725eff0673bb43eff
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-11 06:44:59 +0800

    贝叶斯

---
+++ src/main/java/com/wds/dataalgorithms/nbc/JavaNBCSpark.java
@@ -0,0 +1,26 @@
+package com.wds.dataalgorithms.nbc;
+
+import edu.umd.cloud9.io.pair.PairOfStrings;
+import org.apache.hadoop.io.DoubleWritable;
+import scala.Tuple2;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 贝叶斯
+ * Created by wangdongsong1229@163.com on 2017/9/10.
+ */
+public class JavaNBCSpark {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static List<Tuple2<PairOfStrings, DoubleWritable>> toWritableList(Map<Tuple2<String, String>, Double> PT){
+
+        return null;
+    }
+
+
+}
