b6e2308b269a609d5c71f9685cfb741367bac718
wangdongsong1229 <wangdongsong1229@163.com>
2017-09-12 22:32:53 +0800

    Step4、5、6

--- src/main/java/com/wds/dataalgorithms/nbc/JavaNBCSpark.java
+++ src/main/java/com/wds/dataalgorithms/nbc/JavaNBCSpark.java
@@ -3,12 +3,15 @@ package com.wds.dataalgorithms.nbc;
 import com.wds.dataalgorithms.util.SparkUtil;
 import edu.umd.cloud9.io.pair.PairOfStrings;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 贝叶斯
@@ -28,10 +31,27 @@ public class JavaNBCSpark {
         JavaSparkContext ctx = SparkUtil.createJavaSparkContext("java nbc");
 
         //Step4 读取训练数据
+        JavaRDD<String> training = ctx.textFile(trainingDataFilename, 1);
+        training.saveAsTextFile("/output/javaNBCSpark/output1");
 
         //Step5 对训练数据的所有元素实现map函数
+        JavaPairRDD<Tuple2<String, String>, Integer> pairs = training.flatMapToPair((rec) ->{
+            String[] tokens = rec.split(",");
+            int classificationindex = tokens.length - 1;
+            final String theClassification = tokens[classificationindex];
+            int end = classificationindex - 1;
+            List<Tuple2<Tuple2<String, String>, Integer>> result = IntStream.range(0, end).mapToObj((i) -> {return new Tuple2<String, String>("CLASS", theClassification);}).map((k) -> {return new Tuple2<Tuple2<String, String>, Integer>(k, 1);}).collect(Collectors.toList());
+            result.add(new Tuple2<Tuple2<String, String>, Integer>(new Tuple2<String, String>("CLASS", theClassification), 1));
+
+            return result.iterator();
+        });
+        pairs.saveAsTextFile("/output/javaNBCSpark/output/2");
 
         //Step6 对训练数据的所有元素实现reduce函数
+        JavaPairRDD<Tuple2<String, String>, Integer> counts = pairs.reduceByKey((i1, i2) -> {
+            return i1 + i2;
+        });
+        counts.saveAsTextFile("/output/javaNBCSpark/output/3");
 
         //Step7  收集归约数据为Map
 
